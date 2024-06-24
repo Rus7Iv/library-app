4028f15f637cf19e69340e05a008a8337d934e3f
Ruslan Ivanov <rusivary@gmail.com>
2024-06-24 19:46:48 +0400

    frontend | fixed display pagination

--- frontend/src/app/books/page.tsx
+++ frontend/src/app/books/page.tsx
@@ -59,34 +59,36 @@ const Books = () => {
                 ) : books.length === 0 ? (
                   <div>No books found</div>
                 ) : (
-                  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                    {books.map((book) => (
-                      <div key={book.id} className="relative group">
-                        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 group-hover:-rotate-6 sm:rounded-xl opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out"></div>
-                        <div className="relative z-10 p-4 border rounded-xl shadow bg-white">
-                          <Image src={`/api/covers/${book.cover}`} alt={book.title} width={100} height={100} className="mb-4" priority/>
-                          <h2 className="text-xl font-bold">{book.title}</h2>
-                          <p>{book.description}</p>
+                  <>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+                      {books.map((book) => (
+                        <div key={book.id} className="relative group">
+                          <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 group-hover:-rotate-6 sm:rounded-xl opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out"></div>
+                          <div className="relative z-10 p-4 border rounded-xl shadow bg-white">
+                            <Image src={`/api/covers/${book.cover}`} alt={book.title} width={500} height={100} className="mb-4" priority/>
+                            <h2 className="text-xl font-bold">{book.title}</h2>
+                            <p>{book.description}</p>
+                          </div>
                         </div>
+                      ))}
+                    </div>
+                    <div className="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7 justify-center flex">
+                      <div className="flex space-x-4">
+                        <button onClick={() => setPage(page - 1)} disabled={page === 1} className={`inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white ${page === 1 ? 'bg-indigo-700' : 'bg-indigo-600 hover:bg-indigo-700'}`}>Previous</button>
+                        {pageNumbers.map(number => (
+                          <button 
+                            key={number} 
+                            onClick={() => setPage(number)} 
+                            className={`inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white ${page === number ? 'bg-green-600 hover:bg-green-700' : 'bg-indigo-600 hover:bg-indigo-700'}`}
+                          >
+                            {number}
+                          </button>
+                        ))}
+                        <button onClick={() => setPage(page + 1)} disabled={page === totalPages} className={`inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white ${page === totalPages ? 'bg-indigo-700' : 'bg-indigo-600 hover:bg-indigo-700'}`}>Next</button>
                       </div>
-                    ))}
-                  </div>
+                    </div>
+                  </>
                 )}
-              </div>
-              <div className="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7 justify-center flex">
-                <div className="flex space-x-4">
-                  <button onClick={() => setPage(page - 1)} disabled={page === 1} className={`inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white ${page === 1 ? 'bg-indigo-700' : 'bg-indigo-600 hover:bg-indigo-700'}`}>Previous</button>
-                  {pageNumbers.map(number => (
-                    <button 
-                      key={number} 
-                      onClick={() => setPage(number)} 
-                      className={`inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white ${page === number ? 'bg-green-600 hover:bg-green-700' : 'bg-indigo-600 hover:bg-indigo-700'}`}
-                    >
-                      {number}
-                    </button>
-                  ))}
-                  <button onClick={() => setPage(page + 1)} disabled={page === totalPages} className={`inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white ${page === totalPages ? 'bg-indigo-700' : 'bg-indigo-600 hover:bg-indigo-700'}`}>Next</button>
-                </div>
               </div>
             </div>
           </div>
