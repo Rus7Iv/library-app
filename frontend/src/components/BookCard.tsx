40e23f949441361978ef0df14cc74b8a3121f7cd
Ruslan Ivanov <rusivary@gmail.com>
2024-06-25 19:00:39 +0400

    frontend | added scrollbar in desc in BookCard

--- frontend/src/components/BookCard.tsx
+++ frontend/src/components/BookCard.tsx
@@ -33,11 +33,15 @@ export const BookCard = ({ book }: BookCardProps) => {
         className={`absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:rounded-xl opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out ${hovered ? '-rotate-90 -translate-y-1/4 bg-cyan-400' : 'group-hover:-rotate-6'}`}
         style={{ transformOrigin: 'bottom' }}
       >
-        {hovered && <p className="absolute inset-0 text-white p-4 rotate-90 -translate-x-1/2 -translate-y-1 w-[250px]">{book.description}</p>}
+        {hovered && (
+            <div className="absolute flex inset-0 text-white p-4 w-[250px] h-[120px] overflow-y-auto" style={{ transform: 'translateY(45%) translateX(-20%) rotate(90deg)' }}>
+                <p>{book.description}</p>
+            </div>
+        )}
       </div>
       <div className="relative p-4 border rounded-xl shadow bg-white h-[300px]">
-        <Image src={`/api/covers/${book.cover}`} alt={book.title} width={500} height={100} className="mb-4" priority style={{ maxHeight: '150px', objectFit: 'cover' }}/>
-        <h2 className="text-xl font-bold">{book.title}</h2>
+        <Image src={`/api/covers/${book.cover}`} alt={book.title} className="mb-4 h-[150px] object-cover" width={500} height={200} priority />
+        <h2 className="text-xl font-bold w-full text-center">{book.title}</h2>
       </div>
     </div>
   );
